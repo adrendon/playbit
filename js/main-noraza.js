903885a2aa983ffa8b025f99c8ef56e489c59321
alexis rendón <alexis9229@hotmail.com>
2016-10-10 23:18:08 -0500

    main

--- js/main-noraza.js
+++ js/main-noraza.js
@@ -9,13 +9,13 @@ jQuery(function ($) {
     
     (function () {
 
-        $(".home-body").backstretch([ "images/slider/slider1.jpg","images/slider/slider2.jpg","images/slider/slider3.jpg"], {fade: 750,duration: 3000});
+        // $(".home-body").backstretch([ "images/slider/slider1.jpg","images/slider/slider2.jpg","images/slider/slider3.jpg"], {fade: 750,duration: 3000});
     
-        $(".body-multiple").backstretch([], {fade: 750,duration: 3000});
+        // $(".body-multiple").backstretch(["images/noraza/slider/slider.jpg"], {fade: 750,duration: 3000});
 
-        $(".body-band").backstretch([ "images/slider/slider4.jpg","images/slider/slider5.jpg"], {fade: 750,duration: 3000});
+        // $(".body-band").backstretch([ "images/slider/slider4.jpg","images/slider/slider5.jpg"], {fade: 750,duration: 3000});
 
-        $(".body-personal").backstretch([ "images/slider/slider6.jpg","images/slider/slider2.jpg"], {fade: 750,duration: 3000});
+        // $(".body-personal").backstretch([ "images/slider/slider6.jpg","images/slider/slider2.jpg"], {fade: 750,duration: 3000});
 
     }());
 
